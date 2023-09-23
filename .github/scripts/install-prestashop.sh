6317bb0c5cdb556c4f721ebb40c7023a0572fe48
Roman Kříž <roman@kriz.io>
2023-09-23 18:51:38 +0200

    fix(test): lang issues

--- .github/scripts/install-prestashop.sh
+++ .github/scripts/install-prestashop.sh
@@ -10,6 +10,15 @@ if [ ! -d vendor2/PrestaShop ]; then
             # install deps
             composer install --prefer-dist --no-progress --no-ansi --no-interaction
 
+            # Fixes from https://github.com/retailcrm/prestashop-module/blob/c531f9b1249eef2ffbad6eaecc686cada9a975f7/Makefile#L60
+            sed -i 's/throw new Exception/#throw new Exception/g' src/PrestaShopBundle/Install/DatabaseDump.php
+
+            sed -i "s/SymfonyContainer::getInstance()->get('translator')/\\\\Context::getContext()->getTranslator()/g" classes/lang/DataLang.php
+	        cat classes/lang/DataLang.php | grep -A 3 -B 3 'this->translator = '
+
+	        sed -i "s/SymfonyContainer::getInstance()->get('translator')/\\\\Context::getContext()->getTranslator()/g" classes/Language.php
+	        cat classes/Language.php | grep -A 3 -B 3 'translator = '
+
             # Clean up needed for StarterTheme tests
             mysql -u root --password=password --port ${MYSQL_PORT} -e "DROP DATABASE IF EXISTS \`prestashop\`;"
 
--- tests/Unit/bootstrap.php
+++ tests/Unit/bootstrap.php
@@ -22,3 +22,8 @@ if (file_exists(__DIR__ . '/../../vendor2/PrestaShop/tests-legacy')) {
 require_once __DIR__ . '/../../vendor2/PrestaShop/config/config.inc.php';
 require_once __DIR__ . '/../../vendor2/PrestaShop/config/defines_uri.inc.php';
 require_once __DIR__ . '/../../vendor2/PrestaShop/init.php';
+
+require_once __DIR__ . '/../../miguel.php';
+
+$module = new Miguel();
+$module->install();
