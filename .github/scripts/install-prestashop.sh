5c0918a0fc6801d8f650e86e75d2976f400406ae
Roman Kříž <roman@kriz.io>
2023-09-23 16:51:39 +0200

    ci: set password for mysql

--- .github/scripts/install-prestashop.sh
+++ .github/scripts/install-prestashop.sh
@@ -10,8 +10,8 @@ if [ ! -d vendor2/PrestaShop ]; then
             # install prestashop
             composer install --prefer-dist --no-progress --no-ansi --no-interaction
 
-            sed -i "s/mysql -u root/mysql -u root --port ${MYSQL_PORT}/g" travis-scripts/install-prestashop
-			sed -i "s/--db_server=127.0.0.1 --db_name=prestashop/--db_server=127.0.0.1:${MYSQL_PORT} --db_name=prestashop --db_user=root/g" travis-scripts/install-prestashop
+            sed -i "s/mysql -u root/mysql -u root --password=password --port ${MYSQL_PORT}/g" travis-scripts/install-prestashop
+			sed -i "s/--db_server=127.0.0.1 --db_name=prestashop/--db_server=127.0.0.1:${MYSQL_PORT} --db_name=prestashop --db_user=root --db_password=password/g" travis-scripts/install-prestashop
 
             echo "new version of travis-scripts/install-prestashop script:"
             cat travis-scripts/install-prestashop
