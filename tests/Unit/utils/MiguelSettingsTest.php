42533638ced2111100ceda05af63732acc5f92a6
Roman Kříž <roman@kriz.io>
2023-09-23 12:29:22 +0200

    fix(test): add file back

---
+++ tests/Unit/utils/MiguelSettingsTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * 2023 Servantes
+ *
+ * This file is licenced under the Software License Agreement.
+ * With the purchase or the installation of the software in your application
+ * you accept the licence agreement.
+ *
+ * You must not modify, adapt or create derivative works of this source code
+ *
+ *  @author Pavel Vejnar <vejnar.p@gmail.com>
+ *  @copyright  2022 - 2023 Servantes
+ *  @license LICENSE.txt
+ */
+
+namespace Tests\Unit\utils;
+
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../../../src/utils/miguel-settings.php';
+
+final class MiguelSettingsTest extends TestCase
+{
+    public function testSaveValue()
+    {
+        $this->assertFalse(Configuration::get(MiguelSettings::API_ENABLE_KEY));
+
+        MiguelSettings::setEnabled(true);
+
+        $this->assertFalse(Configuration::get(MiguelSettings::API_ENABLE_KEY));
+    }
+}
