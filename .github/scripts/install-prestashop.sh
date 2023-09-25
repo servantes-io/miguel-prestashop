82c505a2de0988d5dcd8b8fda5d4858f733a0d4e
Roman Kříž <roman@kriz.io>
2023-09-25 12:21:34 +0200

    ci: remove sed

--- .github/scripts/install-prestashop.sh
+++ .github/scripts/install-prestashop.sh
@@ -4,12 +4,12 @@ set -ex
 
 pushd vendor2/PrestaShop > /dev/null
     # Fixes from https://github.com/retailcrm/prestashop-module/blob/c531f9b1249eef2ffbad6eaecc686cada9a975f7/Makefile#L60
-    sed -i 's/throw new Exception/#throw new Exception/g' src/PrestaShopBundle/Install/DatabaseDump.php
+    # sed -i 's/throw new Exception/#throw new Exception/g' src/PrestaShopBundle/Install/DatabaseDump.php
 
-    sed -i "s/SymfonyContainer::getInstance()->get('translator')/\\\\Context::getContext()->getTranslator()/g" classes/lang/DataLang.php
+    # sed -i "s/SymfonyContainer::getInstance()->get('translator')/\\\\Context::getContext()->getTranslator()/g" classes/lang/DataLang.php
     cat classes/lang/DataLang.php | grep -A 3 -B 3 'this->translator = '
 
-    sed -i "s/SymfonyContainer::getInstance()->get('translator')/\\\\Context::getContext()->getTranslator()/g" classes/Language.php
+    # sed -i "s/SymfonyContainer::getInstance()->get('translator')/\\\\Context::getContext()->getTranslator()/g" classes/Language.php
     cat classes/Language.php | grep -A 3 -B 3 'translator = '
 
     # Clean up needed for StarterTheme tests
