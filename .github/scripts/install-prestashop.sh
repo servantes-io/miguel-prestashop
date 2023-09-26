5e5d93baa8c78214995617b096665473d729011f
Roman Kříž <roman@kriz.io>
2023-09-26 12:42:24 +0200

    ci: add sed back

--- .github/scripts/install-prestashop.sh
+++ .github/scripts/install-prestashop.sh
@@ -4,17 +4,21 @@ set -ex
 
 pushd vendor2/PrestaShop > /dev/null
     # Fixes from https://github.com/retailcrm/prestashop-module/blob/c531f9b1249eef2ffbad6eaecc686cada9a975f7/Makefile#L60
-    # sed -i 's/throw new Exception/#throw new Exception/g' src/PrestaShopBundle/Install/DatabaseDump.php
+    sed -i 's/throw new Exception/#throw new Exception/g' src/PrestaShopBundle/Install/DatabaseDump.php
 
-    # sed -i "s/SymfonyContainer::getInstance()->get('translator')/\\\\Context::getContext()->getTranslator()/g" classes/lang/DataLang.php
+    sed -i "s/SymfonyContainer::getInstance()->get('translator')/\\\\Context::getContext()->getTranslator()/g" classes/lang/DataLang.php
     cat classes/lang/DataLang.php | grep -A 3 -B 3 'this->translator = '
 
-    # sed -i "s/SymfonyContainer::getInstance()->get('translator')/\\\\Context::getContext()->getTranslator()/g" classes/Language.php
+    sed -i "s/SymfonyContainer::getInstance()->get('translator')/\\\\Context::getContext()->getTranslator()/g" classes/Language.php
     cat classes/Language.php | grep -A 3 -B 3 'translator = '
 
+    sed -i "s/\$sfContainer->get('translator')/\\\\Context::getContext()->getTranslator()/g" classes/Language.php
+
     # Clean up needed for StarterTheme tests
-    mysql -u root --password=password --port ${MYSQL_PORT} -h 127.0.0.1 -e "DROP DATABASE IF EXISTS \`prestashop\`;"
-    mysql -u root --password=password --port ${MYSQL_PORT} -h 127.0.0.1 -e "DROP DATABASE IF EXISTS \`test_prestashop\`;"
+    mysql -u root --password=password --port ${MYSQL_PORT} -h 127.0.0.1 -e "
+      DROP DATABASE IF EXISTS \`prestashop\`;
+      DROP DATABASE IF EXISTS \`test_prestashop\`;
+    "
 
     # Remove cache
     rm -rf var/cache/*
