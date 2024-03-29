3c38242dbc5f4e2b5352d9e59de6b290856ea4ce
Roman Kříž <roman@kriz.io>
2024-03-29 11:32:18 +0700

    refactor: store license header in one file + autogenerate index.php files

--- .github/scripts/build-zip.sh
+++ .github/scripts/build-zip.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 
+set -e
+
+# Function to remove space after the first PHPDoc comment
+remove_space_after_phpdoc() {
+    file="$1"
+    echo "Removing space after PHPDoc comment in $file"
+
+    # Check if the file exists
+    if [ ! -f "$file" ]; then
+        echo "File $file does not exist."
+        exit 1
+    fi
+
+    # Find the first PHPdoc and remove spaces after it
+    perl -CSD -i -pe 'BEGIN{undef $/;} s/\*\/\n\n/*\/\n/smg;' "$file"
+}
+
 tmpdir=$(mktemp -d)
-ignore=".git .github .idea .vscode .gitignore .php-cs-fixer.dist.php .php-cs-fixer.cache composer.lock tests run doc vendor vendor2 docker-compose.yml docker-compose.test.yml Makefile *.zip"
+ignore=".git .github .idea .vscode .gitignore .php-cs-fixer.dist.php .php-cs-fixer.cache composer.lock assets tests run doc vendor2 docker-compose.yml docker-compose.test.yml Makefile *.zip"
 result="$(PWD)/miguel.zip"
 
 rm -rf "$result"
@@ -10,6 +27,28 @@ mkdir -p $tmpdir/miguel
 rsync -a --exclude=.git --exclude=run --exclude=vendor --exclude=vendor2 "." "${tmpdir}/miguel/"
 
 pushd $tmpdir > /dev/null
+    if [[ -f "miguel/composer.json" ]]; then
+        pushd miguel > /dev/null
+            composer install
+
+            composer exec autoindex
+            cp src/index.php .
+
+            composer exec header-stamp -- --license=assets/license-header.txt --exclude=.github,node_modules,vendor,tests,_dev,run,composer.json
+
+            for file in $(find . -type f -name "*.php" ! -path "./vendor/*"); do
+                remove_space_after_phpdoc "$file"
+            done
+            cat index.php
+
+            composer exec php-cs-fixer fix -- --using-cache=no
+
+            rm -rf vendor/
+
+            composer install --no-dev
+        popd > /dev/null
+    fi
+
     for i in $ignore; do
         rm -rf miguel/$i
     done
--- tests/Unit/bootstrap.php
+++ tests/Unit/bootstrap.php
@@ -1,17 +1,5 @@
 <?php
-/**
- * 2024 Servantes
- *
- * This file is licenced under the Software License Agreement.
- * With the purchase or the installation of the software in your application
- * you accept the licence agreement.
- *
- * You must not modify, adapt or create derivative works of this source code
- *
- *  @author Roman Kříž <roman.kriz@servantes.cz>
- *  @copyright  2022 - 2024 Servantes
- *  @license LICENSE.txt
- */
+
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 if (file_exists(__DIR__ . '/../../vendor2/PrestaShop/tests-legacy')) {
