4905cb0a3f9e4a2e5c3c2dde181270edd7b76663
Roman Kříž <roman@kriz.io>
2024-06-14 15:14:43 +0200

    feat(test): add tests for products.php

---
+++ tests/Unit/ProductsTest.php
@@ -0,0 +1,85 @@
+<?php
+
+use Miguel\Utils\MiguelSettings;
+use PHPUnit\Framework\TestCase;
+use Tests\Unit\Utility\ContextMocker;
+
+class ProductsTest extends TestCase
+{
+    /**
+     * @var ContextMocker
+     */
+    protected $contextMocker;
+
+    private $previousErrorReportingSetting;
+
+    protected function setUp(): void
+    {
+        $this->contextMocker = new ContextMocker();
+        $this->contextMocker->mockContext();
+
+        MiguelSettings::reset();
+        unset($_SERVER['Authorization']);
+
+        $this->previousErrorReportingSetting = error_reporting(E_ALL ^ E_WARNING ^ E_DEPRECATED);
+
+        // Suppress output to console
+        $this->setOutputCallback(function() {});
+
+        parent::setUp();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->contextMocker->resetContext();
+
+        error_reporting($this->previousErrorReportingSetting);
+
+        parent::tearDown();
+    }
+
+    public function testInvalidToken()
+    {
+        // PREPARE
+        $_SERVER['Authorization'] = 'Bearer 1234';
+
+        MiguelSettings::setEnabled(true);
+        MiguelSettings::save(MiguelSettings::API_TOKEN_PRODUCTION_KEY, '0000');
+
+        // TEST
+        $output = $this->sut();
+
+        // ASSERT
+        $expected_output = json_encode([
+            'result' => false,
+            'debug' => '',
+            'error' => [
+                'code' => 'api_key.invalid',
+                'message' => 'API key invalid',
+            ],
+        ]);
+        $this->assertJsonStringEqualsJsonString($output, $expected_output);
+    }
+
+    public function testActualData()
+    {
+        // PREPARE
+        $_SERVER['Authorization'] = 'Bearer 1234';
+
+        MiguelSettings::setEnabled(true);
+        MiguelSettings::save(MiguelSettings::API_TOKEN_PRODUCTION_KEY, '1234');
+
+        // TEST
+        $output = $this->sut();
+
+        // ASSERT
+        $json = json_decode($output, true);
+        $this->assertIsArray($json['products']);
+    }
+
+    private function sut(): string
+    {
+        include __DIR__ . '/../../products.php';
+        return $this->getActualOutput();
+    }
+}
