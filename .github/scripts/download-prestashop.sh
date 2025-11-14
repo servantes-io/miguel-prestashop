3632ab7ba027ddba82949402fb488323c70e42e0
Roman Kříž <roman@kriz.io>
2025-11-14 14:09:05 +0100

    fix(ci): do not make assets everytime

--- .github/scripts/download-prestashop.sh
+++ .github/scripts/download-prestashop.sh
@@ -7,13 +7,13 @@ if [ ! -d vendor2/PrestaShop ]; then
     pushd vendor2 > /dev/null
         # download prestashop
         git clone --depth 1 --branch $PRESTASHOP_VERSION https://github.com/PrestaShop/PrestaShop
+
+        pushd PrestaShop > /dev/null
+            echo "* Installing PrestaShop dependencies ...";
+            composer install --prefer-dist --no-progress --no-ansi --no-interaction
+
+            echo "* Building PrestaShop assets ...";
+            make assets
+        popd > /dev/null
     popd > /dev/null
 fi
-
-pushd vendor2/PrestaShop > /dev/null
-    # install deps
-    composer install --prefer-dist --no-progress --no-ansi --no-interaction
-
-    echo "* Building PrestaShop assets ...";
-    make assets
-popd > /dev/null
