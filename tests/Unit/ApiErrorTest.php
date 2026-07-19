dbc3353524bd82a5a195b68eb8fb38cf9c7985cb
Roman Kříž <roman@kriz.io>
2026-07-19 08:49:50 +0200

    feat: add resourceNotFound and methodNotAllowed API errors

---
+++ tests/Unit/ApiErrorTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use Miguel\Utils\MiguelApiError;
+use PHPUnit\Framework\TestCase;
+
+class ApiErrorTest extends TestCase
+{
+    public function testResourceNotFound()
+    {
+        $error = MiguelApiError::resourceNotFound('widgets');
+
+        $this->assertSame('resource.not_found', $error->getCode());
+        $this->assertSame('Resource widgets not found', $error->getMessage());
+    }
+
+    public function testMethodNotAllowed()
+    {
+        $error = MiguelApiError::methodNotAllowed('DELETE');
+
+        $this->assertSame('method.not_allowed', $error->getCode());
+        $this->assertSame('Method DELETE not allowed', $error->getMessage());
+    }
+}
